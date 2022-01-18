00d4caf93aeb509c886c3e94864dd069aff359b6
laihanbin <512412344@qq.com>
2022-01-18 14:10:09 +0800

    fix: run params

--- app/libs/init/index.ts
+++ app/libs/init/index.ts
@@ -1,8 +1,10 @@
+import { initRunParams } from './initRunParams';
 import { initUser } from './initUser';
 import { initSetting } from './initSetting';
 import { initRole } from './initRole';
 
 export {
+  initRunParams,
   initUser,
   initRole,
   initSetting
