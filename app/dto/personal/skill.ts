786475202a81448c161318fb578e69aee45cd2ad
laihanbin <512412344@qq.com>
2022-03-25 16:33:35 +0800

    fix: skill dto

--- app/dto/personal/skill.ts
+++ app/dto/personal/skill.ts
@@ -1,4 +1,3 @@
-import * as _ from 'lodash';
 import {
   Length,
   IsString,
@@ -51,15 +50,14 @@ class SkillBatchDto {
   isAll?: boolean;
 
   @IsOptional()
-  @IsArray({ message: 'id集合必须是个 Array<number> 类型' })
+  @IsArray({ message: 'id集合必须是个 Array<string> 类型' })
   @ArrayMinSize(1, { message: 'id集合不能为空' })
   @IsInt({
     each: true,
-    message: 'id集合必须是个 Array<number> 类型'
+    message: 'id集合必须是个 Array<string> 类型'
   })
-  @Transform(v => _.isArray(v) && v.map(val => parseInt(val)), { toClassOnly: true })
   @Expose()
-  ids?: number[];
+  ids?: string[];
 }
 
 export class BatchSkillMountDto extends SkillBatchDto {
